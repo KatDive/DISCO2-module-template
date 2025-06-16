c60759b87a0af213515e9d6256e03f8e6e3e1c94
MahmoodSeoud <Mahmoodseoud@gmail.com>
2025-06-16 20:05:08 +0100

    demosaic_module_cpp from template.

--- src/demosaic_module.cpp
+++ src/demosaic_module.cpp
@@ -1,10 +1,12 @@
 #include "module.h"
 #include "util.h"
+#include <opencv2/opencv.hpp>
+#include <string.h>
 
 /* Define custom error codes */
 enum ERROR_CODE {
     MALLOC_ERR = 1,
-    PLACEHOLDER = 2,
+    OPENCV_ERR = 2,
 };
 
 /* START MODULE IMPLEMENTATION */
@@ -13,34 +15,81 @@ void module()
     /* Get number of images in input batch */
     int num_images = get_input_num_images();
 
-    /* Retrieve module parameters by name (defined in config.yaml) */
-    int param_1 = get_param_bool("param_name_1");
-    int param_2 = get_param_int("param_name_2");
-    float param_3 = get_param_float("param_name_3");
-    char *param_4 = get_param_string("param_name_4");
+    /* Retrieve module parameters by name (defined in config.yaml) (None for this module)*/
 
-    /* Example code for iterating a pixel value at a time */
+    /* Process each image in the batch */
     for (int i = 0; i < num_images; ++i)
     {
         Metadata *input_meta = get_metadata(i);
         int height = input_meta->height;
         int width = input_meta->width;
-        int channels = input_meta->channels;
         int timestamp = input_meta->timestamp;
         int bits_pixel = input_meta->bits_pixel;
         char *camera = input_meta->camera;
-
-        /* Get custom metadata values */
-        // int example_bool = get_custom_metadata_bool(input_meta, "example_bool");
-        // int int_example = get_custom_metadata_int(input_meta, "example_int");
-        // float example_float = get_custom_metadata_float(input_meta, "example_float");
-        // char *example_string = get_custom_metadata_string(input_meta, "example_string");
         
+        /* Validate input is single channel */
+        if (input_meta->channels != 1) {
+            signal_error_and_exit(OPENCV_ERR);
+        }
+        
+        /* Validate bits_pixel */
+        if (bits_pixel < 8 || bits_pixel > 16) {
+            signal_error_and_exit(OPENCV_ERR);
+        }
+
+        /* Get bayer pattern from metadata or use parameter */
+        char bayer_pattern[16];
+        strcpy(bayer_pattern, "GRBG"); // Default
+
         unsigned char *input_image_data;
         size_t size = get_image_data(i, &input_image_data);
 
-        /* Define temporary output image */
-        unsigned char *output_image_data = (unsigned char *)malloc(size);
+        /* Create OpenCV Mat from raw data */
+        cv::Mat rawImage;
+        
+        if (bits_pixel <= 8) {
+            rawImage = cv::Mat(height, width, CV_8UC1, input_image_data);
+        } else {
+            rawImage = cv::Mat(height, width, CV_16UC1, input_image_data);
+            if (bits_pixel == 12) {
+                /* Mask to 12-bit values */
+                uint16_t* ptr = (uint16_t*)rawImage.data;
+                size_t total_pixels = width * height;
+                for (size_t j = 0; j < total_pixels; j++) {
+                    ptr[j] = ptr[j] & 0x0FFF;
+                }
+            }
+        }
+
+        /* Convert to 8-bit for demosaicing */
+        cv::Mat rawImage8bit;
+        try {
+            if (bits_pixel <= 8) {
+                rawImage.copyTo(rawImage8bit);
+            } else {
+                rawImage.convertTo(rawImage8bit, CV_8U, 255.0/((1 << bits_pixel) - 1));
+            }
+            
+            /* Apply vertical flip */
+            cv::Mat processedImage;
+            cv::flip(rawImage8bit, processedImage, 0);  // 0 means vertical flip
+
+            /* Determine OpenCV color code for demosaicing */
+            int cv_color_code;
+            /* Using GRBG bayer pattern */
+            cv_color_code = cv::COLOR_BayerGR2BGR;
+            
+            /* Perform demosaicing */
+            cv::Mat demosaicedImage;
+            cv::cvtColor(processedImage, demosaicedImage, cv_color_code);
+        } catch (const cv::Exception& e) {
+            free(input_image_data);
+            signal_error_and_exit(OPENCV_ERR);
+        }
+
+        /* Get output data and size */
+        size_t output_size = demosaicedImage.total() * demosaicedImage.elemSize();
+        unsigned char *output_image_data = (unsigned char *)malloc(output_size);
 
         /* Check for malloc error */
         if (output_image_data == NULL)
@@ -48,46 +97,27 @@ void module()
             signal_error_and_exit(MALLOC_ERR);
         }
 
-        for (int y = 0; y < height; ++y)
-        {
-            for (int x = 0; x < width; ++x)
-            {
-                for (int c = 0; c < channels; ++c)
-                {
-                    /* Accessing pixel data for image i, at position (x, y) and channel c */
-                    int pixel_index = y * (width * channels) +
-                                      x * channels + c;
-                    unsigned char pixel_value = input_image_data[pixel_index];
+        /* Copy OpenCV Mat data to output buffer */
+        memcpy(output_image_data, demosaicedImage.data, output_size);
 
-                    /* Perform any processing here */
-                    /* Example: You can manipulate pixel_value or perform any operation */
-
-                    /* If you want to write back processed value, you can do something like this: */
-                    output_image_data[pixel_index] = pixel_value;
-                }
-            }
-        }
-        
-        /* Create image metadata before appending */
+        /* Create image metadata for output */
         Metadata new_meta = METADATA__INIT;
-        new_meta.size = size;
-        new_meta.width = width;
-        new_meta.height = height;
-        new_meta.channels = channels;
+        new_meta.size = output_size;
+        new_meta.width = demosaicedImage.cols;
+        new_meta.height = demosaicedImage.rows;
+        new_meta.channels = demosaicedImage.channels();  // Should be 3 for BGR
         new_meta.timestamp = timestamp;
-        new_meta.bits_pixel = bits_pixel;
+        new_meta.bits_pixel = 8;  // Output is 8-bit
         new_meta.camera = camera;
 
-        /* Add custom metadata key-value */
-        add_custom_metadata_bool(&new_meta, "example_bool", true);
-        add_custom_metadata_int(&new_meta, "example_int", 20);
-        add_custom_metadata_float(&new_meta, "example_float", 20.5);
-        add_custom_metadata_string(&new_meta, "example_string", "TEST");
+        /* Add custom metadata */
+        add_custom_metadata_string(&new_meta, "demosaic_bayer_pattern", bayer_pattern);
+        add_custom_metadata_string(&new_meta, "color_space", "BGR");
 
         /* Append the image to the result batch */
-        append_result_image(output_image_data, size, &new_meta);
+        append_result_image(output_image_data, output_size, &new_meta);
 
-        /* Remember to free any allocated memory */
+        /* Free allocated memory */
         free(input_image_data);
         free(output_image_data);
     }
